5f8c14260c8dba54385683745e9ccde05a087909
Nishant <nishantshreshth.13@gmail.com>
2016-05-13 17:23:52 +0530

    [Nishant] wingify test to be removed

---
+++ static/test.js
@@ -0,0 +1,12 @@
+<script type="text/javascript">
+(function(p,u,s,h){
+    p._pcq=p._pcq||[];
+    p._pcq.push(['_currentTime',Date.now()]);
+    s=u.createElement('script');
+    s.type='text/javascript';
+    s.async=true;
+    s.src='https://cdn.pushcrew.com/js/ab984d148f1ae609cc2a110c35997d6c.js';
+    h=u.getElementsByTagName('script')[0];
+    h.parentNode.insertBefore(s,h);
+})(window,document);
+</script>
