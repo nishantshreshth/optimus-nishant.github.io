56769f3818d70bb1fef12b7d182693ab1b03ad01
Nishant <nishantshreshth.13@gmail.com>
2016-05-18 13:44:01 +0530

    [Nishant] minor change for wingify

--- static/test.js
+++ static/test.js
@@ -1,4 +1,4 @@
-<script type="text/javascript">
+
 (function(p,u,s,h) {
    p._pcq = p._pcq || [];
    p._pcq.push(['_currentTime', Date.now()]);
@@ -6,4 +6,4 @@
    s.src = 'https://cdn.pushcrew.com/js/280530484c3d5fcb829fdf998e183a16.js';
    h = u.getElementsByTagName('script')[0]; h.parentNode.insertBefore(s, h);
 })(window,document);
-</script>
+
