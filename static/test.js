f181f76bd79aa1cd9019f42ba26b158b233280c7
Nishant <nishantshreshth.13@gmail.com>
2016-05-23 11:46:27 +0530

    [Nishant] Wingify final test1

--- static/test.js
+++ static/test.js
@@ -5,7 +5,7 @@
     s=u.createElement('script');
     s.type='text/javascript';
     s.async=true;
-    s.src='https://cdn.pushcrew.com/js/ab984d148f1ae609cc2a110c35997d6c.js';
+    s.src='https://cdn.pushcrew.com/js/280530484c3d5fcb829fdf998e183a16.js';
     h=u.getElementsByTagName('script')[0];
     h.parentNode.insertBefore(s,h);
 })(window,document);
