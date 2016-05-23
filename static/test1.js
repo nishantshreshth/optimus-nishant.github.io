4718e5becf9fff997e217fe7df2e726a241784d9
Nishant <nishantshreshth.13@gmail.com>
2016-05-23 12:02:12 +0530

    [Nishant] Wingify final test12

---
+++ static/test1.js
@@ -0,0 +1,10 @@
+(function(p,u,s,h){
+    p._pcq=p._pcq||[];
+    p._pcq.push(['_currentTime',Date.now()]);
+    s=u.createElement('script');
+    s.type='text/javascript';
+    s.async=true;
+    s.src='https://cdn.pushcrew.com/js/791bca04458a6f795485c7a66386ab4d.js';
+    h=u.getElementsByTagName('script')[0];
+    h.parentNode.insertBefore(s,h);
+})(window,document);
